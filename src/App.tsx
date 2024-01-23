b7915fd27781fc7b5f40198f003a2eb4beb96d4b
bimendra <bimendra.85@gmail.com>
2024-01-23 20:37:42 +0800

    Install react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
+import { BiCart } from "react-icons/bi";
 
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <BiCart />
     </div>
   );
 };
